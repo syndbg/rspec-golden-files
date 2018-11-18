9015aa268e740740187d79df660f1df1d1da6514
Anton Antonov <anton.synd.antonov@gmail.com>
2018-11-19 00:28:51 +0200

    Init

---
+++ spec/rspec/golden/files_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Rspec::Golden::Files do
+  it "has a version number" do
+    expect(Rspec::Golden::Files::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
